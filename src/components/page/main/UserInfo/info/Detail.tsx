96e050022afd9cd6169e3cd7cea2f6c1158ef6dd
nang-gu <namgyu1020@naver.com>
2024-09-12 20:37:06 +0900

    [feat] 일기작성부분 퍼블리싱작업

--- src/components/page/main/UserInfo/info/Detail.tsx
+++ src/components/page/main/UserInfo/info/Detail.tsx
@@ -1,36 +1,92 @@
 import CommonHeader from "../../../../common/CommonHeader";
 import "../../../../../App.css";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { useCallback, useEffect, useState } from "react";
+import DiaryHeader from "../../../../common/diary/DiaryHeader";
+import DiaryButton from "../../../../common/diary/DiaryButton";
+import EmotionItem from "../../../../common/diary/EmotionItem";
 
 const Detail = () => {
     const { date } = useParams();
     console.log(date);
+    const [selectedDate, setSelectDate] = useState("");
+    const [emotion, setEmotion] = useState(3);
+    const navigate = useNavigate();
 
+    //
+    const emotionList = [
+        {
+            emotion_id: 1,
+            emotion_img: process.env.PUBLIC_URL + `/assets/emotion1.png`,
+            emotion_descript: "완전 좋음",
+        },
+        {
+            emotion_id: 2,
+            emotion_img: process.env.PUBLIC_URL + `/assets/emotion2.png`,
+            emotion_descript: "좋음",
+        },
+        {
+            emotion_id: 3,
+            emotion_img: process.env.PUBLIC_URL + `/assets/emotion3.png`,
+            emotion_descript: "그럭 저럭",
+        },
+        {
+            emotion_id: 4,
+            emotion_img: process.env.PUBLIC_URL + `/assets/emotion4.png`,
+            emotion_descript: "나쁨",
+        },
+        {
+            emotion_id: 5,
+            emotion_img: process.env.PUBLIC_URL + `/assets/emotion5.png`,
+            emotion_descript: "완전 나쁨",
+        },
+    ];
+    //
+
+    useEffect(() => {
+        if (!date) return;
+        setSelectDate(date);
+    }, [date]);
+
+    const handleClickEmotion = useCallback((score: any) => {
+        console.log(score);
+        setEmotion(score);
+    }, []);
     return (
         <div>
             <CommonHeader>
                 <div className="DiaryEditor">
+                    <DiaryHeader
+                        headText={"오늘의 기억"}
+                        leftChild={
+                            <DiaryButton
+                                text={"뒤로가기"}
+                                onClick={() => navigate(-1)}
+                            />
+                        }
+                        rightChild={<DiaryButton text={"삭제하기"} />}
+                    />
                     <section>
                         <h4>오늘은 언제인가요?</h4>
                         <div className="input_box">
                             <input
                                 className="input_date"
                                 type="date"
-                                // value={selectDate}
-                                // onChange={(e) => setSelectDate(e.target.value)}
+                                value={selectedDate}
+                                onChange={(e) => setSelectDate(e.target.value)}
                             />
                         </div>
                         <section>
                             <h4>오늘의 감정</h4>
                             <div className="input_box emotion_list_wrapper">
-                                {/* {emotionList.map((item) => (
-                                <EmotionItem
-                                    key={item.emotion_id}
-                                    {...item}
-                                    onClick={handleClickEmotion}
-                                    isSelected={item.emotion_id === emotion}
-                                />
-                            ))} */}
+                                {emotionList.map((item) => (
+                                    <EmotionItem
+                                        key={item.emotion_id}
+                                        {...item}
+                                        onClick={handleClickEmotion}
+                                        isSelected={item.emotion_id === emotion}
+                                    />
+                                ))}
                             </div>
                         </section>
                         <section>
